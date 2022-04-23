5707f7476800971f72b4661d1d27cc0d2f294c5f
piotryfon <31383071+piotryfon@users.noreply.github.com>
2022-04-23 15:31:34 +0200

    -Dodaj walidację

--- WebAppJsonCrud/Models/User.cs
+++ WebAppJsonCrud/Models/User.cs
@@ -5,9 +5,14 @@ namespace WebAppJsonCrud.Models
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is required")]
+        [StringLength(50, MinimumLength = 3)]
         public string Name { get; set; }
 
         [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "The Email Address is required")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Email Address is not valid")]
         public string EmailAddress { get; set; }
     }
 
